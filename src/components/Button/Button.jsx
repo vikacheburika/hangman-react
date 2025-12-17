146a8da93eaeecaab1b8c8008fa31086b437542f
vikacheburika <vikacheburika@gmail.com>
2025-12-17 22:22:43 +0300

    Tried styling a button with bootstrap css and it changed the overall appearance too

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,11 @@
 import "./Button.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Button = ({onClick}) => {
 
     return(
         <>
-        <button className="button-wrong" onClick={onClick}>
+        <button className="button-wrong" onClick={onClick} type="button" type="button" className="btn btn-primary btn-lg">
             GUESS WRONG
         </button>
         </>
